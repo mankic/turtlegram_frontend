b8e886b187b80321541df255699db498ccc35a3c
mankic <miic0803@gmail.com>
2022-05-18 00:04:50 +0900

    수정 - 회원가입버튼 id 명칭 변경 추가 - 이메일 입력오류 alert 추가

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-async function handleSignin() {
+async function handleSignup() {
 
     const signupData = {
         email : document.getElementById('floatingInput').value,
@@ -6,9 +6,23 @@ async function handleSignin() {
     }
     
 
-    const response = await fetch('http://127.0.0.1:5002/signup', {
+    if (signupData.email == ''){
+        alert('이메일을 입력해주세요.')
+    }
+    else if (signupData.email.indexOf('@') == -1) {    // indexOf 포함하고있는 문자의 인덱스를 반환한다. 없으면 -1 반환.
+        alert('이메일 형식이 아닙니다.')
+    }
+    else {
+        const response = await fetch('http://127.0.0.1:5002/signup', {  // app.py 서버 주소
         method: 'POST',
         body: JSON.stringify(signupData)
-    })
-    console.log(response)   
+        })
+        console.log(response)   
+
+        if (response.status == 200){
+            alert('회원가입 완료!')
+            window.location.replace('http://127.0.0.1:5500/index.html')
+        }
+    }
+ 
 }
